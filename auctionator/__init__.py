59464b64b816fa8868c65d18f90d0ac0014013df
Mihai Ionescu <mihai.ionescu@gespit.com>
2023-08-04 16:52:27 +0300

    WIP .env configuration integration

--- auctionator/__init__.py
+++ auctionator/__init__.py
@@ -2,36 +2,26 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-# from config import Config
+load_dotenv("../.env")
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-
-    app.config.from_mapping(
-        SECRET_KEY=os.environ.get("SECRET_KEY") or "key",
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
+    app = Flask(__name__, instance_relative_config=False)
 
     if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_pyfile("../config.py")
     else:
         app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @app.route("/hello")
-    def hello():
-        return "hello world"
+    @app.route("/")
+    def index():
+        return f'SECRET_KEY = { app.config.get("SECRET_KEY") }'
 
     from . import db
 
     db.init_app(app)
-    from . import auth
+    # from . import auth
 
-    app.register_blueprint(auth.bp)
+    # app.register_blueprint(auth.bp)
 
     return app
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-SECRET_KEY = "dev-config"
+from os import environ
+
+SECRET_KEY = environ.get("SECRET_KEY")
+API_KEY = environ.get("API_KEY")
+DB_NAME = environ.get("DB_NAME")
+DB_USER = environ.get("DB_USER")
+DB_PASSWORD = environ.get("DB_PASSWORD")
